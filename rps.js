d0bb77f5c1dbe4073c0d18fe6d7094cb8f9df910
Logan Lickfield <l.lickfield@gmail.com>
2024-08-28 11:07:29 -0400

    Interface complete! All buttons were working, but the score appending was not adding to the display scorebox. More styling could be done to center the added result divs. It is mostly html with spacing styling done.

--- rps.js
+++ rps.js
@@ -14,35 +14,59 @@ function getComputerChoice() {
     return compChoice;
 }
 
+
+let result = document.createElement('p');
+let roundResult = document.querySelector(".choices");
+let humScoreDisplay = document.querySelector(".human-score");
+let compScoreDisplay = document.querySelector(".comp-score");
+
+let buttons = document.querySelectorAll(".btn");
+let humChoice = document.querySelector("p.hum-choice");
+let compChoice = document.querySelector("p.comp-choice");
+//Add event listener to call playRound() with the playerSelection
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        humanChoice = button.id;
+        playRound(humanChoice, comp = getComputerChoice());
+        //Add div to display results, change console.log() to DOM Methods
+        humChoice.textContent = "Your Choice: " + humanChoice;
+        compChoice.textContent = "Computer Choice: " + comp;
+    });
+});
+//Display running score, announce a winner once a player reaches 5 points
 function playRound(humanChoice, compChoice) {
     if (humanChoice === compChoice) {
-        let draw = document.createElement('p');
-        draw.textContent = "Draw!";
-        drawScore = document.querySelector(".choices");
-        drawScore.appendChild(draw);
+        result.textContent = "Draw!";
+        roundResult.appendChild(result);
     } else if (compChoice == "rock" && humanChoice == "paper") {
-        console.log("You win! Paper beats Rock.");
-        ++humanScore;
+        result.textContent = "You win! Paper beats Rock.";
+        roundResult.appendChild(result);
+        humScoreDisplay.textContent = parseInt(humScoreDisplay.textContent) + 1;
         printScore();
     } else if (compChoice == "rock" && humanChoice == "scissors") {
-        console.log("You lose! Rock beats Scissors.");
-        ++computerScore;
+        result.textContent = "You lose! Rock beats Scissors.";
+        roundResult.appendChild(result);
+        compScoreDisplay.textContent = parseInt(compScoreDisplay.textContent) + 1;
         printScore();
     } else if (compChoice == "paper" && humanChoice == "rock") {
-        console.log("You lose! Paper beats Rock.");
-        ++computerScore;
+        result.textContent = "You lose! Paper beats Rock.";
+        roundResult.appendChild(result);
+        compScoreDisplay.textContent = parseInt(compScoreDisplay.textContent) + 1;
         printScore();
     } else if (compChoice == "paper" && humanChoice == "scissors") {
-        console.log("You win! Scissors beat Paper");
-        ++humanScore;
+        result.textContent = "You win! Scissors beat Paper";
+        roundResult.appendChild(result);
+        humScoreDisplay.textContent = parseInt(humScoreDisplay.textContent) + 1;
         printScore();
     } else if (compChoice == "scissors" && humanChoice == "rock") {
-        console.log("You win! Rock beats scissors.");
-        ++humanScore;
+        result.textContent = "You win! Rock beats scissors.";
+        roundResult.appendChild(result);
+        humScoreDisplay.textContent = parseInt(humScoreDisplay.textContent) + 1;
         printScore();
     } else if (compChoice == "scissors" && humanChoice == "paper") {
-        console.log("You lose! Scissors beat Paper.");
-        ++computerScore;
+        result.textContent = "You lose! Scissors beat Paper.";
+        roundResult.appendChild(result);
+        compScoreDisplay.textContent = parseInt(compScoreDisplay.textContent) + 1;
         printScore();
     }
 }
@@ -61,21 +85,3 @@ function playGame() {
         console.log("Game Result: Draw.")
     }
 }
-
-//Add event listener to call playRound() with the playerSelection
-let buttons = document.querySelectorAll(".btn");
-let humChoice = document.querySelector("p.hum-choice");
-let compChoice = document.querySelector("p.comp-choice");
-
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        humanChoice = button.id;
-        playRound(humanChoice, comp = getComputerChoice());
-        humChoice.textContent = humanChoice;
-        compChoice.textContent = comp;
-
-    });
-});
-
-//Add div to display results, change console.log() to DOM Methods
-//Display running score, announce a winner once a player reaches 5 points
